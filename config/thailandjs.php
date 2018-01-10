f7733a6de1b772b90881b1d20495ef694c5a17f1
baraear <baraear@gmail.com>
2018-01-10 20:36:27 +0700

    Add publish any publishable assets from package

---
+++ config/thailandjs.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'use-mix' => true,
+];
--- src/ThailandJSServiceProvider.php
+++ src/ThailandJSServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Baraear\ThailandJS\Providers;
+namespace Baraear\ThailandJS;
 
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
 
 class ThailandJSServiceProvider extends ServiceProvider
@@ -13,7 +14,28 @@ class ThailandJSServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app instanceof LaravelApplication) {
+            $this->publishes([
+                __DIR__ . '/../config/thailandjs.php' => $this->app->configPath() . '/thailandjs.php',
+            ], 'config');
+            if (config('thailandjs.use-mix')) {
+                $this->publishes([
+                    __DIR__ . '/../libraries/JQL.min.js' => $this->app->resourcePath() . "/assets/vendors/laravel-thailand-js/JQL.min.js",
+                    __DIR__ . '/../libraries/typeahead.bin.js' => $this->app->resourcePath() . "/assets/vendors/laravel-thailand-js/typeahead.bin.js",
+                    __DIR__ . '/../libraries/zip.min.js' => $this->app->resourcePath() . "/assets/vendors/laravel-thailand-js/zip.min.js",
+                    __DIR__ . '/../libraries/jquery.Thailand.min.js' => $this->app->resourcePath() . "/assets/vendors/laravel-thailand-js/jquery.Thailand.min.js",
+                    __DIR__ . '/../libraries/jquery.Thailand.min.css' => $this->app->resourcePath() . "/assets/vendors/laravel-thailand-js/jquery.Thailand.min.css",
+                ], 'resources');
+            } else {
+                $this->publishes([
+                    __DIR__ . '/../libraries/JQL.min.js' => $this->app->publicPath() . "/js/laravel-thailand-js/JQL.min.js",
+                    __DIR__ . '/../libraries/typeahead.bin.js' => $this->app->publicPath() . "/js/laravel-thailand-js/typeahead.bin.js",
+                    __DIR__ . '/../libraries/zip.min.js' => $this->app->publicPath() . "/js/laravel-thailand-js/zip.min.js",
+                    __DIR__ . '/../libraries/jquery.Thailand.min.js' => $this->app->publicPath() . "/js/laravel-thailand-js/jquery.Thailand.min.js",
+                    __DIR__ . '/../libraries/jquery.Thailand.min.css' => $this->app->publicPath() . "/css/laravel-thailand-js/jquery.Thailand.min.css",
+                ], 'resources');
+            }
+        }
     }
 
     /**
@@ -23,6 +45,11 @@ class ThailandJSServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if ($this->app instanceof LaravelApplication) {
+            $this->mergeConfigFrom(
+                __DIR__.'/../config/thailandjs.php',
+                'thailandjs'
+            );
+        }
     }
 }

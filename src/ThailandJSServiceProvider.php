64721e6b3c555b606dcf145756a29f9579a8b648
baraear <baraear@gmail.com>
2018-01-10 19:44:48 +0700

    Create a package service provider

---
+++ src/ThailandJSServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Baraear\ThailandJS\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ThailandJSServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}

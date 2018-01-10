023eabce85faac3f9cf747ddbcfa143de2a5ce23
baraear <baraear@gmail.com>
2018-01-10 20:11:37 +0700

    Edit script for update jquery.Thailand.js libraries

--- scripts/update-js-lib.sh
+++ scripts/update-js-lib.sh
@@ -6,3 +6,6 @@ git submodule update --remote
 echo "Copying jquery.Thailand.js library into the project"
 cp ./jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.js ./libraries/jquery.Thailand.min.js
 cp ./jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.css ./libraries/jquery.Thailand.min.css
+cp ./jquery.Thailand.js/jquery.Thailand.js/dependencies/zip.js/zip.js ./libraries/zip.min.js
+cp ./jquery.Thailand.js/jquery.Thailand.js/dependencies/JQL.min.js ./libraries/JQL.min.js
+cp ./jquery.Thailand.js/jquery.Thailand.js/dependencies/typeahead.bundle.js ./libraries/typeahead.bin.js

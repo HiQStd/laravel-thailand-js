4889288ca45744bf31cadb4e8af82c88c01bb534
baraear <baraear@gmail.com>
2018-01-11 13:00:19 +0700

    Update script for update dependencies

--- scripts/update-js-lib.sh
+++ scripts/update-js-lib.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-echo "Updating jquery.Thailand.js"
+echo "Updating dependencies"
 git submodule update --remote
 
-echo "Copying jquery.Thailand.js library into the project"
-cp ./jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.js ./libraries/jquery.Thailand.min.js
-cp ./jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.css ./libraries/jquery.Thailand.min.css
-cp ./jquery.Thailand.js/jquery.Thailand.js/dependencies/zip.js/zip.js ./libraries/zip.min.js
-cp ./jquery.Thailand.js/jquery.Thailand.js/dependencies/JQL.min.js ./libraries/JQL.min.js
-cp ./jquery.Thailand.js/jquery.Thailand.js/dependencies/typeahead.bundle.js ./libraries/typeahead.bin.js
+echo "Build jQuery"
+cd dependencies/jquery && npm run build
+cd ../../
+
+echo "Copying dependencies into project libraries"
+cp ./dependencies/jquery/dist/jquery.min.js ./libraries/jquery.min.js
+cp ./dependencies/JQL.js/dist/jql.min.js ./libraries/jql.min.js
+cp ./dependencies/zip.js/WebContent/zip.js ./libraries/zip.js
+cp ./dependencies/uikit/dist/js/uikit.js ./libraries/uikit.js
+cp ./dependencies/uikit/dist/css/uikit.css ./libraries/uikit.css
+cp ./dependencies/typeahead.js/dist/typeahead.bundle.js ./libraries/typeahead.bundle.js
+cp ./dependencies/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.js ./libraries/jquery.Thailand.min.js
+cp ./dependencies/jquery.Thailand.js/jquery.Thailand.js/dist/jquery.Thailand.min.css ./libraries/jquery.Thailand.min.css

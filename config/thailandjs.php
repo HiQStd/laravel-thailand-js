e63b5b049dc6582dc91dc979cc9313a82b506747
baraear <baraear@gmail.com>
2018-01-11 16:18:41 +0700

    Add configuration comments

--- config/thailandjs.php
+++ config/thailandjs.php
@@ -1,9 +1,20 @@
 <?php
 
 return [
+    /*
+     * หากคุณใช้ laravel-mix ในการ compile asset files.
+     */
     'use-mix' => false,
+
     'path' => [
+        /*
+         * ระบุ path ของไฟล์ .js ในกรณีที่คุณไม่ได้ใช้ laravel-mix
+         */
         'js' => '/js/laravel-thailand-js',
+
+        /*
+         * ระบุ path ของไฟล์ .css ในกรณีที่คุณไม่ได้ใช้ laravel-mix
+         */
         'css' => '/css/laravel-thailand-js',
     ],
 ];
